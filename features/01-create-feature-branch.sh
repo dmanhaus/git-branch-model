bd963924d15d4f7cc2db7e8062b1443793df6ef6
Dave Hausler <david.hausler@cigna.com>
2017-09-07 09:42:41 -0400

    Added conditional argument checks

--- features/01-create-feature-branch.sh
+++ features/01-create-feature-branch.sh
@@ -1,1 +1,6 @@
-git checkout -b $1 develop
+if [ -z "$1"]
+  then 
+    echo "MISSING ARGUMENT - Must supply name of new feature as argument, e.g.: bash features/01-create-feature-branch.sh [new-feature-name]"
+  else
+    git checkout -b $1 develop
+fi
--- releases/04-merge-release-branch-master.sh
+++ releases/04-merge-release-branch-master.sh
@@ -1,4 +1,10 @@
-git checkout master
-git merge --no-ff release-$1
-git tag -a $1
-git push origin $1
+if [ -z "$1"]
+  then 
+    echo "MISSING ARGUMENT - Must supply name of release branch as argument, e.g.: bash releases/04-merge-release-branch-master.sh release-[major release number].[minor release number]"
+  else
+    git checkout master
+    git merge --no-ff release-$1
+    git tag -a $1
+    git push origin $1
+fi
+
