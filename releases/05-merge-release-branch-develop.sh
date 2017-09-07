46b1c01b7721393fce705afc541e4e9ae895b169
Dave Hausler <david.hausler@cigna.com>
2017-09-07 11:28:10 -0400

    fixed 'missing argument' messages to reflect proper argument syntax

--- releases/05-merge-release-branch-develop.sh
+++ releases/05-merge-release-branch-develop.sh
@@ -1,6 +1,6 @@
 if [ -z "$1" ]
   then 
-    echo "MISSING ARGUMENT - Must supply name of release branch as argument, e.g.: bash releases/05-merge-release-branch-develop.sh release-[major release number].[minor release number]"
+    echo "MISSING ARGUMENT - Must supply version number of release branch as argument, e.g.: bash releases/05-merge-release-branch-develop.sh [major release number].[minor release number]"
   else
     git checkout develop
     git merge --no-ff release-$1
