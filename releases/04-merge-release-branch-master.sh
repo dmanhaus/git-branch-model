f192f5503225be223ce093bd5741c07bd5b4c5f1
Dave Hausler <david.hausler@cigna.com>
2017-09-07 09:48:22 -0400

    Added missing space before right bracket in if condition

--- releases/04-merge-release-branch-master.sh
+++ releases/04-merge-release-branch-master.sh
@@ -1,4 +1,4 @@
-if [ -z "$1"]
+if [ -z "$1" ]
   then 
     echo "MISSING ARGUMENT - Must supply name of release branch as argument, e.g.: bash releases/04-merge-release-branch-master.sh release-[major release number].[minor release number]"
   else
